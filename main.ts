051d07682c0d509b4c70ee8478b2364c2c5893c8
conjuringwxq <1794594886@qq.com>
2020-10-15 17:09:36 +0800

    electron支持

---
+++ main.ts
@@ -0,0 +1,38 @@
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+
+const IsDev = process.env.NODE_ENV === 'development';
+const IsProd = process.env.NODE_ENV === 'production';
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1260,
+    height: 810,
+    webPreferences: {
+      nodeIntegration: true,
+      // devTools: false,
+    },
+    title: 'music',
+    resizable: false, // 禁止缩放窗体
+  });
+  if (IsDev) {
+    win.loadURL('http://localhost:8000');
+    win.webContents.openDevTools();
+  } else if (IsProd) {
+    win.loadFile(path.resolve(__dirname, 'dist', 'index.html'));
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
