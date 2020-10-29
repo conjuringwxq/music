2923077ba55313b5c5809e4ef7e6ce18086483aa
conjuringwxq <1794594886@qq.com>
2020-10-29 14:56:57 +0800

    用户信息页

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect } from 'react';
+import { Space, Row, Col } from 'antd';
+import { FolderAddOutlined, UserOutlined } from '@ant-design/icons';
 import { connect, SingerModelState, useParams } from 'umi';
-import { ConnectProps } from '@/models/connect';
+import { ConnectProps, ConnectState } from '@/models/connect';
+import { Image, Text, RaiseButton } from '@/components/style';
 
 interface ProfileProps extends ConnectProps {
   singer: SingerModelState;
@@ -29,9 +32,38 @@ const Profile: React.FC<ProfileProps> = (props) => {
   }, [dispatch, params.id]);
 
   return (
-    <pre>
-      <code>{JSON.stringify(message, null, 2)}</code>
-    </pre>
+    <>
+      <Space>
+        <Row gutter={[24, 0]}>
+          <Image shape="square" size={200} src={message.picUrl} />
+          <Col flex={1}>
+            <Row gutter={[0, 14]}>
+              <Col span={24}>
+                <Text size={28} bold>
+                  {message.name}
+                </Text>
+              </Col>
+              <Col span={24}>
+                <Space>
+                  <RaiseButton icon={<FolderAddOutlined />}>收藏</RaiseButton>
+                  <RaiseButton icon={<UserOutlined />}>个人主页</RaiseButton>
+                </Space>
+              </Col>
+              <Col span={24}>
+                <Space>
+                  <Text>单曲数:</Text>
+                  <Text color="#c9c9c9">{message.musicSize}</Text>
+                  <Text>专辑数:</Text>
+                  <Text color="#c9c9c9">{message.albumSize}</Text>
+                  <Text>MV数:</Text>
+                  <Text color="#c9c9c9">{message.mvSize}</Text>
+                </Space>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Space>
+    </>
   );
 };
 
